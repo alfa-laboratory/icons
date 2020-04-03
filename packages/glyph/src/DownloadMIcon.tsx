84f7d2f5a2c034fdffd6026a6ee56962cc747051
dmitrsavk <dmitrsavk@yandex.ru>
2020-04-03 15:47:01 +0000

    feat(*): add new icons

---
+++ packages/glyph/src/DownloadMIcon.tsx
@@ -0,0 +1,24 @@
+/**
+ * This is auto-generated file by scripts/generate.ts
+ * Do not edit it manually
+ */
+import React from 'react';
+
+type Props = {
+    className?: string;
+};
+
+export const DownloadMIcon: React.FC<Props> = ({ className }) => (
+    <svg
+        className={className}
+        focusable="false"
+        fill="currentColor"
+        width="24"
+        height="24"
+    >
+        <path d="M0 0h24v24H0V0z" />
+        <path d="M4 19h16v2H4v-2zm7-5.5V3h2v10.5l3.5-3.5 1.5 1.5-6 5.5-6-5.5L7.5 10l3.5 3.5z" />
+    </svg>
+);
+
+export default DownloadMIcon;
