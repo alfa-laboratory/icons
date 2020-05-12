c8e26622a4e3e86ea76a792d7d86cf3e2d12c253
dmitrsavk <dmitrsavk@yandex.ru>
2020-05-12 11:43:55 +0000

    feat(*): add new icons

---
+++ packages/glyph/src/UserMIcon.tsx
@@ -0,0 +1,28 @@
+/**
+ * This is auto-generated file by scripts/generate.ts
+ * Do not edit it manually
+ */
+import React from 'react';
+
+type Props = {
+    className?: string;
+};
+
+export const UserMIcon: React.FC<Props> = ({ className }) => (
+    <svg
+        className={className}
+        focusable="false"
+        fill="currentColor"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+    >
+        <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M16 6a4 4 0 11-8 0 4 4 0 018 0zm-4 6c8.1 0 9 9 9 9H3s.9-9 9-9z"
+        />
+    </svg>
+);
+
+export default UserMIcon;
