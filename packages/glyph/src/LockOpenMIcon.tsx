bfa5ca5de1846ad1dac4057ec370b9c2119fb77d
dmitrsavk <dmitrsavk@yandex.ru>
2020-05-13 08:08:59 +0000

    feat(*): add new icons

---
+++ packages/glyph/src/LockOpenMIcon.tsx
@@ -0,0 +1,28 @@
+/**
+ * This is auto-generated file by scripts/generate.ts
+ * Do not edit it manually
+ */
+import React from 'react';
+
+type Props = {
+    className?: string;
+};
+
+export const LockOpenMIcon: React.FC<Props> = ({ className }) => (
+    <svg
+        className={className}
+        focusable="false"
+        fill="currentColor"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+    >
+        <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M12 1a5 5 0 00-5 5v1h2V6a3 3 0 116 0v4H4v8a4 4 0 004 4h8a4 4 0 004-4v-8h-3V6a5 5 0 00-5-5zm1.5 14.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0z"
+        />
+    </svg>
+);
+
+export default LockOpenMIcon;
