c17fd42e8cccb59687070913fd2eaf606cbe5734
dmitrsavk <dmitrsavk@yandex.ru>
2021-06-22 13:19:23 +0300

    fix ci

--- bin/update-package-version.sh
+++ bin/update-package-version.sh
@@ -3,15 +3,22 @@
 # название подпакета => @alfalab/icons-website => website = package_name
 package_name=$(echo $1 | awk -F- '{print $2}')
 
+PACKAGE_CHANGED=$(lerna changed | grep "$package_name")
+
 LATEST_TAG=$(git describe --tags --abbrev=0)
 DELETED_ICONS=$(git diff --name-only $LATEST_TAG HEAD --diff-filter=D | grep "$package_name/")
 
-if [ -z "$DELETED_ICONS" ]
+if [ -z "$PACKAGE_CHANGED" ]
 then
-    npm version minor
+    # Если не было изменений в пакете, то ничего не делаем
 else
-    npm version major
-fi
+    if [ -z "$DELETED_ICONS" ]
+    then
+        npm version minor
+    else
+        npm version major
+    fi
 
-git add .
-git commit --amend -m "feat(*): add new icons"
+    git add .
+    git commit --amend -m "feat(*): add new icons"
+fi
